8d7bce7a320d7c2d2877e093bc440fb9896d59e5
Fabio Albiero <fabio.albiero@kartera.com>
2024-02-15 12:54:31 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,176 @@
+//difine html elements
+const board = document.getElementById('game-board');
+const instructionText = document.getElementById('instruction-text');
+const logo = document.getElementById('logo');
+const score = document.getElementById('score');
+const hightScoreEl = document.getElementById('hightScore');
+//define game variables;
+const GRID_SIZE = 20;
+let snake = [{x: 10, y:10}];
+let food = generateFood();
+let direction = 'RIGHT';
+let gameInterval;
+let gameSpeedDelay = 200;
+let gameStarted = false;
+let hightScore = 0;
+
+//draw game map, snake, food
+function draw() {
+    board.innerHTML = '';
+    drawSnake();
+    drawFood();
+}
+
+//draw snake
+function drawSnake() {
+    snake.forEach( segment => {
+        const snakeEl = createGameElement('div', 'snake');
+        setPosition(snakeEl, segment);
+        board.appendChild(snakeEl);
+    });
+}
+
+//create a snake or food div
+function createGameElement(tag, className) {
+    const el = document.createElement(tag);
+    el.className = className;
+    return el;
+}
+
+//set the position of snake or food
+function setPosition(el, pos) {
+    el.style.gridColumn = pos.x;
+    el.style.gridRow = pos.y;
+}
+
+//draw food
+function drawFood() {
+    const foodEl = createGameElement('div', 'food');
+    setPosition(foodEl, food);
+    board.appendChild(foodEl);
+}
+
+//generate food
+function generateFood() {
+    const x = Math.floor(Math.random() * GRID_SIZE) + 1;
+    const y = Math.floor(Math.random() * GRID_SIZE) + 1;
+    return { x, y };
+}
+
+//moving the snake
+function move() {
+    const head = {...snake[0]};
+    switch (direction) {
+        case 'RIGHT': 
+            head.x++;
+            break;
+        case 'LEFT': 
+            head.x--;
+            break;
+        case 'UP': 
+            head.y--;
+            break;
+        case 'DOWN': 
+            head.y++;
+            break;
+    }
+    snake.unshift(head);
+    if (head.x === food.x && head.y === food.y) {
+        food = generateFood();
+        updateScore();
+        increaseSpeed();
+        clearInterval(gameInterval); //clear past interval
+        setInterval = setInterval(() => {
+            move();
+            draw();
+        }, gameSpeedDelay);
+    } else {
+        snake.pop();
+    }
+}
+
+function increaseSpeed() {
+    gameSpeedDelay -= gameSpeedDelay > 150 ? 5 : 2;
+}
+
+function checkColision() {
+    const head = snake[0];
+    if (head.x < 1 || head.x > GRID_SIZE ||
+        head.y < 1 || head.y > GRID_SIZE) {
+            resetGame();
+    }
+
+    for (let i = 1; i < snake.length; i++) {
+        if (head.x === snake[i].x || head.y === snake[i].y) {
+            resetGame();
+        }
+    }
+}
+
+function resetGame() {
+    snake = [{x: 10, y: 10}];
+    food = generateFood();
+    direction = 'RIGHT';
+    gameSpeedDelay = 200;
+    gameStarted = false;
+    updateScore();
+    updateHightScore();
+    stopGame();
+}
+
+function updateScore() {
+    const currentScore = snake.length - 1;
+    score.textContent = currentScore.toString().padStart(3, '0');
+}
+
+function updateHightScore() {
+    const currentScore = snake.length - 1;
+    if (currentScore > hightScore) {
+        hightScore = currentScore;
+        hightScoreEl.textContent = hightScore.toString().padStart(3, '0');
+    }
+}
+
+//start game function
+function startGame() {
+    gameStarted = true;
+    instructionText.style.display = 'none';
+    logo.style.display = 'none';
+    gameInterval = setInterval(() => {
+        move();
+        checkColision();
+        draw();
+    }, gameSpeedDelay);
+}
+
+function stopGame() {
+    clearInterval(gameInterval);
+    gameStarted = false;
+    instructionText.style.display = 'block';
+    logo.style.display = 'block';
+}
+
+//keypress event listener
+function handleKeyPress(event) {
+    if ((!gameStarted && event.code === 'Space') ||
+        (!gameStarted && event.key === ' ')) {
+        startGame();
+    } else {
+        switch (event.key) {
+            case 'ArrowUp': 
+                direction = 'UP';
+                break;
+            case 'ArrowDown': 
+                direction = 'DOWN';
+                break;
+            case 'ArrowRight': 
+                direction = 'RIGHT';
+                break;
+            case 'ArrowLeft': 
+                direction = 'LEFT';
+                break;
+        }
+    }
+}
+
+document.addEventListener('keydown', handleKeyPress);
